ccf68d293386351e842bd8c065c3b85386e2ddc1
Teoman Shipahi <aytekoz@yahoo.com>
2018-06-09 22:51:36 -0400

    Getting started with ImageSharp

---
+++ apps/app-ImageSharp/GggImageSharp/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GggImageSharp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
