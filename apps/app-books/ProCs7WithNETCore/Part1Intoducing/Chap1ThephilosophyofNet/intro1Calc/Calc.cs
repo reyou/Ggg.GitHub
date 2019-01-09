b2579280cea00d8fdc4c94b6410c5b6c8c5b72ff
aozdemir <aozdemir@webmd.net>
2019-01-09 14:08:20 -0500

    CTS Enumeration Types

---
+++ apps/app-books/ProCs7WithNETCore/Part1Intoducing/Chap1ThephilosophyofNet/intro1Calc/Calc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro1
+{
+    // This class contains the app's entry point.
+    class Program
+    {
+        static void Main()
+        {
+            Calc c = new Calc();
+            int ans = c.Add(10, 84);
+            Console.WriteLine("10 + 84 is {0}.", ans);
+            // Wait for user to press the Enter key before shutting down.
+            Console.ReadLine();
+        }
+    }
+    // The C# calculator.
+    class Calc
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ apps/app-books/ProCs7WithNETCore/Part1Intoducing/Chap1ThephilosophyofNet/intro2Structs/Intro.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro1
+{
+
+    // A C# structure type.
+    struct Point
+    {
+        // Structures can contain fields.
+        public int XPos, YPos;
+        // Structures can contain parameterized constructors.
+        public Point(int x, int y)
+        {
+            XPos = x;
+            YPos = y;
+        }
+        // Structures may define methods.
+        public void PrintPosition()
+        {
+            Console.WriteLine("({0}, {1})", XPos, YPos);
+        }
+    }
+    // This class contains the app's entry point.
+    class Program
+    {
+        static void Main()
+        {
+            Point point = new Point();
+            point.PrintPosition();
+            point = new Point(10, 20);
+            point.PrintPosition();
+            Console.ReadLine();
+        }
+    }
+
+}
