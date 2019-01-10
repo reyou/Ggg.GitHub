c4bfe406f74dee51eb97fd5eb80bcf5ebd155d54
aozdemir <aozdemir@webmd.net>
2019-01-10 13:44:57 -0500

    intro3DotNetCoreTest

---
+++ apps/app-books/ProCs7WithNETCore/Part1Intoducing/Chap1ThephilosophyofNet/intro3DotNetCoreTest/intro2mstest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace intro2mstest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
