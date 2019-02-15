ad5bcdc6873d1bd42444813fa5366ffd516901a5
aozdemir <aozdemir@webmd.net>
2019-02-15 14:13:05 -0500

    Generic Delegates

---
+++ apps/app-books/ProCs7WithNETCore/Part4AdvancedCSharpProgramming/chap10/intro1Notifications-Using-Delegates/intro/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace intro
+{
+    public class Car
+    {
+        // 1) Define a delegate type.
+        public delegate void CarEngineHandler(string msgForCaller);
+        // 2) Define a member variable of this delegate.
+        private CarEngineHandler listOfHandlers;
+        // 3) Add registration function for the caller.
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listOfHandlers = methodToCall;
+        }
+        // 4) Implement the Accelerate() method to invoke the delegate's
+        // invocation list under the correct circumstances.
+        public void Accelerate(int delta)
+        {
+            // If this car is "dead," send dead message.
+            if (carIsDead)
+            {
+                if (listOfHandlers != null)
+                    listOfHandlers("Sorry, this car is dead...");
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                // Is this car "almost dead"?
+                if (10 == (MaxSpeed - CurrentSpeed) && listOfHandlers != null)
+                {
+                    listOfHandlers("Careful buddy! Gonna blow!");
+                }
+                if (CurrentSpeed >= MaxSpeed)
+                    carIsDead = true;
+                else
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+
+        // Internal state data.
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+        // Is the car alive or dead?
+        private bool carIsDead;
+        // Class constructors.
+        public Car() { }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+    }
+}
---
+++ apps/app-books/ProCs7WithNETCore/Part4AdvancedCSharpProgramming/chap10/intro2Enabling-Multicasting/intro/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Delegates as event enablers *****\n");
+            // First, make a Car object.
+            Car c1 = new Car("SlugBug", 100, 10);
+            // Register multiple targets for the notifications.
+            c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
+            c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent2));
+            // Speed up (this will trigger the events).
+            Console.WriteLine("***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+            Console.ReadLine();
+        }
+        // We now have TWO methods that will be called by the Car
+        // when sending notifications.
+        public static void OnCarEngineEvent(string msg)
+        {
+            Console.WriteLine("\n***** Message From Car Object *****");
+            Console.WriteLine("=> {0}", msg);
+            Console.WriteLine("***********************************\n");
+        }
+        public static void OnCarEngineEvent2(string msg)
+        {
+            Console.WriteLine("=> {0}", msg.ToUpper());
+        }
+    }
+
+}
