eb293f672e98edd0c6214813b2fa09ad29e8d6b5
aozdemir <aozdemir@webmd.net>
2019-04-01 16:04:29 -0400

    Getting Started with EF Core on ASP.NET Core with an Existing Database

---
+++ apps/apps-docs/docs.microsoft.com/ef/core/get-started/aspnetcore/existing-db/intro1/README.md
@@ -0,0 +1,6 @@
+* Title:	Getting Started on ASP.NET Core - Existing Database - EF Core | Microsoft Docs
+  * Url:	https://docs.microsoft.com/en-us/ef/core/get-started/aspnetcore/existing-db
+
+```
+Scaffold-DbContext "Server=(local);Database=Blogging;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
+```
---
+++ apps/apps-docs/docs.microsoft.com/ef/core/get-started/aspnetcore/existing-db/intro1/intro/Models/BloggingContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace intro.Models
+{
+    public partial class BloggingContext : DbContext
+    {
+        public BloggingContext()
+        {
+        }
+
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Blog> Blog { get; set; }
+        public virtual DbSet<Post> Post { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(local);Database=Blogging;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>(entity =>
+            {
+                entity.Property(e => e.Url).IsRequired();
+            });
+
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.HasOne(d => d.Blog)
+                    .WithMany(p => p.Post)
+                    .HasForeignKey(d => d.BlogId);
+            });
+        }
+    }
+}
