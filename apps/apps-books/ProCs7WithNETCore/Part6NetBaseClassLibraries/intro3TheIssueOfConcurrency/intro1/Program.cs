96a4c166237c1cfe9ffc415943ef5a08c7111309
aozdemir <aozdemir@webmd.net>
2019-04-10 14:49:25 -0400

    Synchronizing Threads

---
+++ apps/apps-books/ProCs7WithNETCore/Part6NetBaseClassLibraries/intro3TheIssueOfConcurrency/intro1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace intro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Synchronizing Threads *****\n");
+            Printer p = new Printer();
+            // Make 10 threads that are all pointing to the same
+            // method on the same object.
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(p.PrintNumbers))
+                {
+                    Name = $"Worker thread #{i}"
+                };
+            }
+            // Now start each one.
+            foreach (Thread t in threads)
+                t.Start();
+            Console.ReadLine();
+        }
+    }
+
+    internal class Printer
+    {
+        public void PrintNumbers()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                // Put thread to sleep for a random amount of time.
+                Random r = new Random();
+                Thread.Sleep(1000 * r.Next(5));
+                Console.WriteLine("{0}:{1}, ", Thread.CurrentThread.Name, i);
+            }
+        }
+    }
+}
