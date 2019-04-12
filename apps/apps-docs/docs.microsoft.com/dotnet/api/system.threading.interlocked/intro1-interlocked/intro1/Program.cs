8713901d040d76f38393f308a24aef2d310abf31
aozdemir <aozdemir@webmd.net>
2019-04-12 10:28:38 -0400

    interlocked

---
+++ apps/apps-docs/docs.microsoft.com/dotnet/api/system.threading.interlocked/intro1-interlocked/intro1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+
+namespace intro1
+{
+    class MyInterlockedExchangeExampleClass
+    {
+        //0 for false, 1 for true.
+        private static int _usingResource = 0;
+
+        private const int NumThreadIterations = 5;
+        private const int NumThreads = 10;
+
+        static void Main()
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < NumThreads; i++)
+            {
+                Thread myThread = new Thread(MyThreadProc);
+                myThread.Name = String.Format("Thread{0}", i + 1);
+
+                //Wait a random amount of time before starting next thread.
+                Thread.Sleep(rnd.Next(0, 1000));
+                myThread.Start();
+            }
+        }
+
+        private static void MyThreadProc()
+        {
+            for (int i = 0; i < NumThreadIterations; i++)
+            {
+                UseResource();
+
+                //Wait 1 second before next attempt.
+                Thread.Sleep(1000);
+            }
+        }
+
+        //A simple method that denies reentrancy.
+        static bool UseResource()
+        {
+            //0 indicates that the method is not in use.
+            if (0 == Interlocked.Exchange(ref _usingResource, 1))
+            {
+                Console.WriteLine("{0} acquired the lock", Thread.CurrentThread.Name);
+
+                //Code to access a resource that is not thread safe would go here.
+
+                //Simulate some work
+                Thread.Sleep(500);
+
+                Console.WriteLine("{0} exiting lock", Thread.CurrentThread.Name);
+
+                //Release the lock
+                Interlocked.Exchange(ref _usingResource, 0);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("   {0} was denied the lock", Thread.CurrentThread.Name);
+                return false;
+            }
+        }
+
+    }
+}
